3d69587d8e2f5f892f51808c4537fa44f7d0042c
Gabriel Bobato <gabrielbobato.rb@gmail.com>
2026-04-06 21:17:55 -0300

    Meu primeiro commit do script Python2

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,8 +1,1 @@
-print("meu primeiro commit")
-git config --global user.name "Gabriel Bobato"
-git config --global user.email "gabrielbobato.rb@gmail.com"
-git remote add origin https://github.com/gabrielbobatorb-pixel/teste.git
-git branch -M main
-git add .
-git commit -m "Meu primeiro commit do script Python"
-git push -u origin main
+print("meu primeiro commit2")
